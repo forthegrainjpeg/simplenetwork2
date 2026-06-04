781ba6da372b94048ccc5caff580d67f0755d8c7
forthegrainjpeg <167659942+forthegrainjpeg@users.noreply.github.com>
2026-06-03 20:55:52 -0700

    added a easter egg as an answer response

--- src/main/java/org/example/simplenetwork2/Messages.java
+++ src/main/java/org/example/simplenetwork2/Messages.java
@@ -16,10 +16,71 @@ public class Messages {
         messages.add("me too!");
 
         messages.add("yay!!!");
-        messages.add("According to all known laws of aviation, " +
-                "there is no way a bee should be able to fly. " +
-                "Its wings are too small to get its fat little body off the ground. " +
-                "The bee, of course, flies anyway because bees don't care what humans think is impossible.");
+        messages.add("Somebody once told me the world is gonna roll me\n" +
+                "I ain't the sharpest tool in the shed\n" +
+                "She was looking kind of dumb with her finger and her thumb\n" +
+                "In the shape of an \"L\" on her forehead\n" +
+                "Well, the years start comin' and they don't stop comin'\n" +
+                "Fed to the rules and I hit the ground runnin'\n" +
+                "Didn't make sense not to live for fun\n" +
+                "Your brain gets smart, but your head gets dumb\n" +
+                "So much to do, so much to see\n" +
+                "So, what's wrong with taking the backstreets?\n" +
+                "You'll never know if you don't go (go)\n" +
+                "You'll never shine if you don't glow\n" +
+                "Hey now, you're an all-star\n" +
+                "Get your game on, go play\n" +
+                "Hey now, you're a rock star\n" +
+                "Get the show on, get paid\n" +
+                "(And all that glitters is gold)\n" +
+                "Only shootin' stars break the mold\n" +
+                "It's a cool place, and they say it gets colder\n" +
+                "You're bundled up now, wait 'til you get older\n" +
+                "But the meteor men beg to differ\n" +
+                "Judging by the hole in the satellite picture\n" +
+                "The ice we skate is gettin' pretty thin\n" +
+                "The water's gettin' warm, so you might as well swim\n" +
+                "My world's on fire, how 'bout yours?\n" +
+                "That's the way I like it, and I'll never get bored\n" +
+                "Hey now, you're an all-star\n" +
+                "Get your game on, go play\n" +
+                "Hey now, you're a rock star\n" +
+                "Get the show on, get paid\n" +
+                "(All that glitters is gold)\n" +
+                "Only shootin' stars break the mold\n" +
+                "Go for the moon\n" +
+                "(Go, go, go) go for the moon\n" +
+                "(Go, go, go) go for the moon\n" +
+                "Go (go), go for the moon\n" +
+                "Hey now, you're an all-star\n" +
+                "Get your game on, go play\n" +
+                "Hey now, you're a rock star\n" +
+                "Get the show on, get paid\n" +
+                "(And all that glitters is gold)\n" +
+                "Only shooting stars\n" +
+                "Somebody once asked, \"Could I spare some change for gas?\n" +
+                "I need to get myself away from this place\"\n" +
+                "I said, \"Yep, what a concept, I could use a little fuel myself\n" +
+                "And we could all use a little change\"\n" +
+                "Well, the years start comin' and they don't stop comin'\n" +
+                "Fed to the rules and I hit the ground runnin'\n" +
+                "Didn't make sense not to live for fun\n" +
+                "Your brain gets smart, but your head gets dumb\n" +
+                "So much to do, so much to see\n" +
+                "So, what's wrong with taking the backstreets?\n" +
+                "You'll never know if you don't go (go!)\n" +
+                "You'll never shine if you don't glow\n" +
+                "Hey now, you're an all-star\n" +
+                "Get your game on, go play\n" +
+                "Hey now, you're a rock star\n" +
+                "Get the show on, get paid\n" +
+                "(And all that glitters is gold)\n" +
+                "Only shootin' stars break the mold\n" +
+                "Only shootin' stars break the mold\n" +
+                "Go for the moon\n" +
+                "Go for the moon\n" +
+                "Go for the moon\n" +
+                "This is how we do it");
         //im so sorry
     }
 
